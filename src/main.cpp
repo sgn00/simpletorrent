9394085736cb437114095f20acf41ed96002a96e
sgn00 <37658877+sgn00@users.noreply.github.com>
2023-04-11 14:55:50 +0800

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "simpletorrent/main.h"
+
+int main() {
+    std::cout << "Hello world" << std::endl;
+}
---
+++ tests/a_test.cpp
@@ -0,0 +1,5 @@
+#include "catch2/catch.hpp"
+
+TEST_CASE("testing") {
+    REQUIRE(1 == 1);
+}
