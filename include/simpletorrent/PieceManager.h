8ffc414906735f5bd48600d150db507264adb18e
sgn00 <37658877+sgn00@users.noreply.github.com>
2023-04-13 18:03:33 +0800

    add piece manager initial impl

--- include/simpletorrent/PieceManager.h
+++ include/simpletorrent/PieceManager.h
@@ -1,8 +1,34 @@
+#include <fstream>
 #include <mutex>
 #include <optional>
 #include <set>
+#include <unordered_map>
+#include <vector>
 
 namespace simpletorrent {
+
+struct BlockRequest {
+  int piece_index;
+  int block_offset;
+  int block_length;
+};
+
+struct Block {
+  int piece_index;
+  int block_offset;
+  std::vector<char> data;
+};
+
+struct BufferPiece {
+  int piece_index;
+  std::vector<char> data;
+  bool empty = true;
+};
+
+struct PieceMetadata {
+  std::string piece_hash;
+  std::vector<uint8_t> blocks_downloaded;
+};
 
 class PieceManager {
  public:
@@ -10,8 +36,23 @@ class PieceManager {
                size_t piece_length, size_t total_length,
                const std::string& output_file);
 
+  std::optional<BlockRequest> request_block(int peer_id);
+
+  bool add_block(int peer_id, const Block& block);
+
+  bool verify_piece(int piece_index);
+
+  void save_piece(int piece_index);
+
+  bool is_download_complete() const;
+
+  std::vector<uint8_t> get_bitfield() const;
+
  private:
-  std::vector<std::string> piece_hashes_;
+  static constexpr size_t DEFAULT_BLOCK_LENGTH = 16384;  // 16 KiB
+
+  static constexpr size_t DEFAULT_BUFFER_SIZE =
+      64;  // max 64 piece parts in memory
 
   size_t piece_length_;
 
@@ -19,7 +60,22 @@ class PieceManager {
 
   std::string output_file_;
 
-  static constexpr size_t default_block_length = 16384;  // 16 KiB, for example
+  std::ofstream output_file_stream_;
+
+  std::mutex pieces_mutex_;
+
+  std::vector<PieceMetadata> pieces_;
+
+  std::set<int> rarest_pieces_;
+
+  std::array<BufferPiece, DEFAULT_BUFFER_SIZE> buffer_;
+
+  std::unordered_map<int, int>
+      piece_buffer_map_;  // maps piece index to buffer index
+
+  void update_rarest_pieces(const std::vector<uint8_t>& peer_bitfield);
+
+  std::optional<BlockRequest> select_next_block(int peer_id);
 };
 
 }  // namespace simpletorrent
--- src/piece_manager.cpp
+++ src/piece_manager.cpp
@@ -1,6 +1,31 @@
-#include <string>
-#include <vector>
-
 #include "simpletorrent/PieceManager.h"
 
-namespace simpletorrent {}
+namespace simpletorrent {
+
+PieceManager::PieceManager(const std::vector<std::string>& piece_hashes,
+                           size_t piece_length, size_t total_length,
+                           const std::string& output_file)
+    : piece_length_(piece_length),
+      total_length_(total_length),
+      output_file_(output_file) {
+  size_t num_pieces = piece_hashes.size();
+  pieces_.reserve(num_pieces);
+  size_t last_piece_length = total_length % piece_length;
+
+  for (size_t i = 0; i < num_pieces; ++i) {
+    size_t current_piece_length =
+        (i == num_pieces - 1 && last_piece_length != 0) ? last_piece_length
+                                                        : piece_length;
+    size_t num_blocks =
+        (current_piece_length + DEFAULT_BLOCK_LENGTH - 1) /
+        DEFAULT_BLOCK_LENGTH;  // rounding up for integer division
+
+    pieces_.emplace_back(PieceMetadata{
+        piece_hashes[i], std::vector<uint8_t>(num_blocks, false)});
+  }
+
+  output_file_stream_.open(output_file,
+                           std::ios::binary | std::ios::in | std::ios::out);
+}
+
+}  // namespace simpletorrent
